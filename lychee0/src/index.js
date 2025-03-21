e829ab8727ad0b693c07ee338dc10bc24d19070b
yaniomar2005 <yaniomar2005@gmail.com>
2025-03-21 23:39:18 +0300

    changes so i deploy the wbesite on github

--- lychee0/src/index.js
+++ lychee0/src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById("root")); // Createroot
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import { BrowserRouter } from 'react-router-dom';
+
+<BrowserRouter basename={process.env.PUBLIC_URL}>
+  <App />
+</BrowserRouter>
+
 
 reportWebVitals();
