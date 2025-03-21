401638692aabbf177a1207dca1a51c2aaf9a283d
yaniomar2005 <yaniomar2005@gmail.com>
2025-03-21 23:52:07 +0300

    chang index.js

--- lychee0/src/index.js
+++ lychee0/src/index.js
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
 
-import { BrowserRouter } from 'react-router-dom';
-
-<BrowserRouter basename={process.env.PUBLIC_URL}>
-  <App />
-</BrowserRouter>
-
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
 
 reportWebVitals();
